4cec3b34b52c41dcfe3d21fa92bb3dda84e681e4
tina513 <dlnaz.ai@hotmail.com>
2016-07-12 11:49:53 -0400

    createOption function for event.js file
    increment name attribute option for createOption function

--- assets/scripts/makesurvey/events.js
+++ assets/scripts/makesurvey/events.js
@@ -7,8 +7,18 @@ const ui = require('./ui');
 // const index = require('../index.js');
 const createPostObject = require('./create-post-object.js');
 
+const onShowMakeSurvey = () => {
+  let link = document.querySelector('link[rel="import"]');
+  let post = link.import.querySelector('#make-survey');
+  let container = document.querySelector('#container');
+  container.appendChild(post.cloneNode(true));
+};
+
+let optionCount = 4;
 const onCreateOption = () => {
-  $( "#survey-fillout" ).append( '<input type="text" name="survey[questions][answers][option]" placeholder="Answer"><br>');
+  let optionNumber = optionCount.toString();
+  $( "#survey-fillout" ).append( '<input type="text" name="survey[questions][answers][' +optionNumber+ ']" placeholder="Answer"><br>');
+  optionCount++;
 };
 
 const onCreateQuestion = (event) => {
@@ -27,6 +37,7 @@ const onCreateSurvey = () => {
 
 
 const addHandlers = () => {
+  $('#make-new-survey').on('click', onShowMakeSurvey);
   $('#add-another-answer').on('click', onCreateOption);
   $('#survey-fillout').on('submit', onCreateQuestion);
   $('#complete-survey-creation').on('click', onCreateSurvey);
