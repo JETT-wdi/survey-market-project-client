8ed78621d1fd2527c902db5ee89bf29c03505242
erinsul <erin_sullivan50@yahoo.com>
2016-07-12 11:52:41 -0400

    Remove console logs
    Removed console logs used for testing

--- assets/scripts/makesurvey/create-post-object.js
+++ assets/scripts/makesurvey/create-post-object.js
@@ -8,7 +8,6 @@ let survey = {
 }; //don't let this reset to blank every time
 
 const createPost = (data) => {
-  console.log(data.surveys.questions);
   survey.survey.questions.push(data.surveys.questions);
   return survey;
 };
