81ec1bea675ec6c431e0c87c9174643710bd902c
tina513 <dlnaz.ai@hotmail.com>
2016-07-12 10:34:07 -0400

    add post request for event handler
    add post request for event handler and index.js file

--- assets/scripts/makesurvey/events.js
+++ assets/scripts/makesurvey/events.js
@@ -4,4 +4,34 @@ const getFormFields = require('../../../lib/get-form-fields');
 
 const api = require('./api');
 const ui = require('./ui');
-const index = require('../index.js');
+// const index = require('../index.js');
+const createPostObject = require('./create-post-object.js');
+
+const onCreateOption = () => {
+  $( "#survey-fillout" ).append( '<input type="text" name="survey[questions][answers][option]" placeholder="Answer"><br>');
+};
+
+const onCreateQuestion = (event) => {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  createPostObject.createPost(data);
+};
+
+const onCreateSurvey = () => {
+  let data = createPostObject.getPost();
+  api.createSurvey(data)
+  .done(ui.Success)
+  .fail(ui.failure);
+};
+
+
+
+const addHandlers = () => {
+  $('#add-').on('click', onCreateOption);
+  $('#survey-fillout').on('submit', onCreateQuestion);
+  $('#complete-survey-creation').on('click', onCreateSurvey);
+};
+
+module.exports = {
+  addHandlers,
+};
